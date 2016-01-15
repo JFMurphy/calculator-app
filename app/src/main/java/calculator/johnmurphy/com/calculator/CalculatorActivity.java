b891faee286d623df9da0dd53dc1fcd31e7d5563
John-Francis Murphy <jfmurphy411@gmail.com>
2016-01-15 16:14:27 +0000

    Changed main activity to CalculatorActivity. Changed layout of buttons to a grid layout.

--- app/src/main/java/calculator/johnmurphy/com/calculator/CalculatorActivity.java
+++ app/src/main/java/calculator/johnmurphy/com/calculator/CalculatorActivity.java
@@ -2,6 +2,9 @@ package calculator.johnmurphy.com.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+    }
+
+    public void addNumber(View view) {
+        TextView textView = (TextView) findViewById(R.id.outputDisplay);
+        Button button = (Button) view;
+
+        textView.append(((Button) view).getText());
     }
 }
