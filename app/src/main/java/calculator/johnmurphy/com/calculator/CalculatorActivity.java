e246083684bb9c00a8328965cea425f041c0a0ec
John-Francis Murphy <jfmurphy411@gmail.com>
2016-01-18 16:23:44 +0000

    Added methods for other buttons.

--- app/src/main/java/calculator/johnmurphy/com/calculator/CalculatorActivity.java
+++ app/src/main/java/calculator/johnmurphy/com/calculator/CalculatorActivity.java
@@ -1,12 +1,14 @@
 package calculator.johnmurphy.com.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private Boolean bracketOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +16,41 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
     }
 
-    public void addNumber(View view) {
-        TextView textView = (TextView) findViewById(R.id.outputDisplay);
+    public void addElement(View view) {
+        EditText input = (EditText) findViewById(R.id.outputDisplay);
         Button button = (Button) view;
 
-        textView.append(((Button) view).getText());
+        input.append(button.getText());
+    }
+
+    public void clearDisplay(View view) {
+        EditText display = (EditText) findViewById(R.id.outputDisplay);
+
+        display.setText("");
+        bracketOpen = false;
+    }
+
+    public void addBrackets(View view) {
+        EditText display = (EditText) findViewById(R.id.outputDisplay);
+
+        if (!bracketOpen) {
+            bracketOpen = true;
+            display.append("(");
+        } else {
+            bracketOpen = false;
+            display.append(")");
+        }
+    }
+
+    public void deleteElement(View view) {
+        String temp;
+        int displayTextLen;
+        EditText display = (EditText) findViewById(R.id.outputDisplay);
+        displayTextLen = display.getText().length();
+
+        if (displayTextLen > 0) {
+            temp = display.getText().toString();
+            display.setText(temp.substring(0, displayTextLen - 1));
+        }
     }
 }
